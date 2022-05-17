ae28b85335a9c0fe5a33129bb1c56bfd6443876e
p41155a <p41155a@gmail.com>
2022-05-17 14:43:02 +0900

    feat: add chapter04 challenge code

--- chapter04/chapter04.playground/Contents.swift
+++ chapter04/chapter04.playground/Contents.swift
@@ -159,6 +159,19 @@ example(of: "prefix(untilOutputFrom:)") {
     }
 }
 
+// MARK: - Challenge: Filter all the things
+example(of: "Challenge") {
+    let numbers = (1...100).publisher
+    
+    numbers
+        .dropFirst(50)
+        .prefix(20)
+        .filter { $0 % 2 == 0 }
+        .sink(receiveCompletion: { print("Completed with: \($0)") },
+              receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
 // MARK: - Support Code
 func example(of description: String,
              action: () -> Void) {
